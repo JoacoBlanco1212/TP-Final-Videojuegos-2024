fa7049f6bc6136942280e8439946a938e3aa86c5
Joaquin Blanco <48386218@est.ort.edu.ar>
2024-11-22 18:34:09 -0300

    Finished. Tengo miedo

--- Assets/InteractableObject.cs
+++ Assets/InteractableObject.cs
@@ -15,7 +15,7 @@ public class InteractableObject : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        activated = false;
     }
 
     // Update is called once per frame
--- Assets/RaycastInteraction.cs
+++ Assets/RaycastInteraction.cs
@@ -6,16 +6,17 @@ using UnityEngine.UI;
 public class RaycastInteraction : MonoBehaviour
 {
     public Transform rayOrigin;
-    public float rayLenght;
+    public float rayLength;
     public LayerMask layer;
     public GameObject uiGO;
     public Text hintText;
     public float hintTime;
     public string defaultHint;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        uiGO.SetActive(false);
     }
 
     // Update is called once per frame
@@ -23,14 +24,19 @@ public class RaycastInteraction : MonoBehaviour
     {
         RaycastHit hit;
         InteractableObject interactable = null;
-        if (Physics.Raycast(rayOrigin.position, rayOrigin.forward, out hit, rayLenght, layer))
+        if (Physics.Raycast(rayOrigin.position, rayOrigin.forward, out hit, rayLength))
         {            
             interactable = hit.collider.GetComponent<InteractableObject>();
             if (interactable)
             {
+                uiGO.SetActive(interactable);
+            }
+            else
+            {
+                uiGO.SetActive(false);
             }
         }
-        uiGO.SetActive(interactable);
+
         if (interactable && Input.GetKeyDown(KeyCode.E))
         {
             string message = "";
@@ -38,10 +44,12 @@ public class RaycastInteraction : MonoBehaviour
             {
                 interactable.PlayObjectAnimation();
                 message = "Succesfully open!";
+                Debug.Log("gero, te quiero mucho.");
             }
             else
             {
                 message = "It's already open!";
+                Debug.Log("gero, my beloved");
             }
             StopAllCoroutines();
             StartCoroutine(ShowHintDuringTime(message));
@@ -52,7 +60,7 @@ public class RaycastInteraction : MonoBehaviour
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawLine(rayOrigin.position, rayOrigin.position + rayOrigin.forward * rayLenght);
+        Gizmos.DrawLine(rayOrigin.position, rayOrigin.position + rayOrigin.forward * rayLength);
     }
 
     IEnumerator ShowHintDuringTime(string hintMessage)
